857db18f3c5201310a21f3142dd67f591a3a15ee
j-sadr <mhmdjwad.sdr@gmail.com>
2025-10-22 19:01:42 +0200

    Projektdateien hinzufügen.

---
+++ koerperfettrechner/main.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+	float bmi, gewicht, groesse;
+
+	printf("Bitte gib Gewicht ein: \n");
+	scanf("%i", &gewicht);
+	printf("Bitte gib G ein: \n");
+	scanf("%i", &gewicht);
+}
